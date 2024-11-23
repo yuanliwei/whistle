0092d9ff81aae2cc1bda99b3aa40100d767e64ff
yuanliwei <891789592@qq.com>
2024-11-23 14:11:54 +0800

    update monaco

--- biz/webui/htdocs/src/js/components/editor/MonacoLoader.js
+++ biz/webui/htdocs/src/js/components/editor/MonacoLoader.js
@@ -1,39 +1,57 @@
-async function getRacePromise(path, name) {
-    try {
-        let code = await (await fetch(path + name)).text()
-        if (code.length > 100) {
-            return [path, name]
-        }
-    } catch (error) {
-        console.warn(error)
-    }
-    console.log('wait forever :', path)
-    return new Promise(() => { })
-}
 
-function getLoaderPromise(path, name) {
-    return new Promise((resolve, reject) => {
-        let node = document.createElement('script')
-        node.type = 'text/javascript'
-        node.async = true
-        node.src = path + name
-        node.onload = () => resolve(path)
-        node.onerror = () => reject()
-        document.head.appendChild(node)
-    })
+
+/** @type{HTMLScriptElement[]} */
+let scriptNodes = []
+
+let KEY_LAST_CDN_NODE = '399bcb0da1a821b8cf06e187022c1778'
+
+/**
+ * @param {(o:object)=>void} resolve
+ * @param {string} path
+ * @param {string} name
+ */
+function loadScript(resolve, path, name) {
+    if (!scriptNodes) { return }
+    let node = document.createElement('script')
+    scriptNodes.push(node)
+    node.type = 'text/javascript'
+    node.async = true
+    node.src = path + name
+    node.onload = () => {
+        /** @type{any} */
+        let require = window['require']
+        if (require?.config && scriptNodes) {
+            console.log('path', path)
+            require.config({ paths: { 'vs': path } })
+            scriptNodes.filter(o => o != node).forEach(o => {
+                o.src = ''
+                o.remove()
+            })
+            scriptNodes = null
+            resolve(require)
+            localStorage.setItem(KEY_LAST_CDN_NODE, [path, name].join('{#}'))
+        }
+    }
+    document.head.appendChild(node)
 }
 
 // https://microsoft.github.io/monaco-editor/index.html
-async function getLoader() {
-    let [path, name] = await Promise.race([
-        getRacePromise('https://cdn.staticfile.org/monaco-editor/0.33.0/min/vs', '/loader.js'),
-        getRacePromise('https://lib.baomitu.com/monaco-editor/0.33.0/min/vs', '/loader.min.js'),
-        getRacePromise('https://cdn.jsdelivr.net/npm/monaco-editor@0.33.0/min/vs', '/loader.js'),
-        getRacePromise('https://microsoft.github.io/monaco-editor/node_modules/monaco-editor/min/vs', '/loader.js'),
-        getRacePromise('https://unpkg.com/monaco-editor@0.33.0/min/vs', '/loader.js'),
-        getRacePromise('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.33.0/min/vs', '/loader.min.js'),
-    ])
-    return getLoaderPromise(path, name)
+async function getRequireLoader() {
+    let requireLoader = await new Promise(async (resolve) => {
+        let [path, name] = (localStorage.getItem(KEY_LAST_CDN_NODE) || '').split('{#}')
+        if (path && name) {
+            loadScript(resolve, path, name)
+            await new Promise((resolve) => setTimeout(resolve, 100))
+        }
+        loadScript(resolve, 'https://cdn.staticfile.net/monaco-editor/0.50.0/min/vs', '/loader.js')
+        loadScript(resolve, 'https://lib.baomitu.com/monaco-editor/0.50.0/min/vs', '/loader.js')
+        loadScript(resolve, 'https://cdn.bootcdn.net/ajax/libs/monaco-editor/0.50.0/min/vs', '/loader.js')
+        loadScript(resolve, 'https://cdn.jsdelivr.net/npm/monaco-editor@0.50.0/min/vs', '/loader.js')
+        loadScript(resolve, 'https://microsoft.github.io/monaco-editor/node_modules/monaco-editor/min/vs', '/loader.js')
+        loadScript(resolve, 'https://unpkg.com/monaco-editor@0.50.0/min/vs', '/loader.js')
+        loadScript(resolve, 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.50.0/min/vs', '/loader.js')
+    })
+    return requireLoader
 }
 
 let monacoPromise
@@ -41,15 +59,12 @@ let monacoPromise
 /**
  * @returns {Promise<import('monaco-editor')>}
  */
-async function load() {
+export async function loadMonaco() {
     if (monacoPromise) return monacoPromise
     monacoPromise = new Promise((resolve) => {
         (async () => {
-            let path = await getLoader()
-            console.log('path', path)
-            let requireLoader = window['require']
-            requireLoader.config({ paths: { 'vs': path } })
-            requireLoader(['vs/editor/editor.main'], () => {
+            let require = await getRequireLoader()
+            require(['vs/editor/editor.main'], () => {
                 const monaco = window['monaco']
                 resolve(monaco)
             })
@@ -58,6 +73,6 @@ async function load() {
     return monacoPromise
 }
 
-export {
-    load
+export default {
+    load: loadMonaco
 }
